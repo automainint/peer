5d4fd3adb32d37c6aa47e88afd35e8f14e1b4c0c
Mitya Selivanov <0x7fffff@guattari.ru>
2022-09-10 22:06:09 +0400

    Project setup

---
+++ source/peer/peer.h
@@ -0,0 +1,12 @@
+#ifndef PEER_PEER_H
+#define PEER_PEER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ source/test/integration/find_package/source/main.c
@@ -0,0 +1,5 @@
+#include <peer/peer.h>
+
+int main(int argc, char **argv) {
+  return 0;
+}
